e5700bfb40f60a8f84b1d2e241f48eba8926e101
LiXianlin <xianlinli@gmail.com>
2018-03-21 12:45:45 +0000

    Update 006.http简单例子.py

--- 006.http简单例子.py
+++ 006.http简单例子.py
@@ -3,10 +3,20 @@
 # -*- coding: utf-8 -*-
 
 import urllib.request
+import urllib.parse
 
-url = "https://github.com/"
+url = "http://www.example.com/?a=1&b=2"
+
+# GET request
 resp = urllib.request.urlopen(url)
-print(resp.status, resp.reason)
+data = resp.read()
+print(data)
 
+
+# POST request
+data = urllib.parse.urlencode({"x": 1, "y": 2})
+data2 = data.encode('utf-8')
+
+resp = urllib.request.urlopen(url, data2)
 data = resp.read()
 print(data)
